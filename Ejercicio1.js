f36a4cb9b348d5a9e35d1e2e863be553ccefa3ea
Duhan Renteria <derh7280@hotmail.com>
2024-02-16 22:14:53 -0500

    Solucion ejercicio 1

---
+++ Ejercicio1.js
@@ -0,0 +1,14 @@
+/*
+Crea variables para guardar los siguientes datos de una persona:
+Nombre
+Apellidos
+Edad
+Ciudad
+Asignar valores a las variables creadas.
+Al finalizar enviar un archivo o imagen con lo descrito anteriormente a tony.ospino@academlo.com
+*/
+var nombre = "Duhan";
+var apellidos ="Renteria Hernandez"
+var edad = 35;
+var ciudad = "Medellin/Colombia";
+console.log("Mi nombre es " + nombre+ " "+ apellidos +"\nSoy aspirante a Academlo y Vivo en " + ciudad);
